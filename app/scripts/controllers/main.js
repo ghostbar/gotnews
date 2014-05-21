99ce4c923f00f52abfddbbc73f65ab610b42e4a3
Jose Luis Rivas <me@ghostbar.co>
2014-05-20 21:47:21 -0500

    quick call for sites

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('newsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.sites = [
+      'http://news.sky.com/feeds/rss/home.xml',
+      'http://feeds.bbci.co.uk/news/rss.xml'
     ];
+
+    $scope.sites.forEach(function (item) {
+      $http({
+        method: 'GET',
+        url: '/get.json',
+        params: {
+          site: item
+        }
+      }).success(function (data, status) {
+        console.log(data);
+        console.log(status);
+      });
+    });
   });
