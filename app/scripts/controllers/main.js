20b83c83a9778899305fb44a309b8af97a6fa1cf
Jose Luis Rivas <me@ghostbar.co>
2014-05-20 22:59:24 -0500

    the full controller working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,21 +2,43 @@
 
 angular.module('newsApp')
   .controller('MainCtrl', function ($scope, $http) {
+    $scope.news = {};
     $scope.sites = [
       'http://news.sky.com/feeds/rss/home.xml',
       'http://feeds.bbci.co.uk/news/rss.xml'
     ];
 
-    $scope.sites.forEach(function (item) {
-      $http({
-        method: 'GET',
-        url: '/get.json',
-        params: {
-          site: item
-        }
-      }).success(function (data, status) {
-        console.log(data);
-        console.log(status);
+    $scope.getSites = function () {
+      $scope.news = {};
+      $scope.sites.forEach(function (item) {
+        $http({
+          method: 'GET',
+          url: '/get.json',
+          params: {
+            site: item
+          }
+        }).success(function (data) {
+          $scope.news[item] = data;
+        });
       });
-    });
+    };
+
+    // getting sites just because we are loading
+    $scope.getSites();
+
+    $scope.removeSite = function (site) {
+      $scope.sites.splice(
+        $scope.sites.indexOf(site),
+        1
+      );
+
+      $scope.getSites();
+    };
+
+    $scope.addSite = function () {
+      $scope.sites.push($scope.newSite);
+      $scope.newSite = '';
+      $scope.getSites();
+    };
+
   });
