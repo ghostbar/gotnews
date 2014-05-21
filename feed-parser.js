31c09255d108396fa318d85bb4d619645ed0743a
Jose Luis Rivas <me@ghostbar.co>
2014-05-20 22:31:17 -0500

    the parser was out of scope and being reused

--- feed-parser.js
+++ feed-parser.js
@@ -2,9 +2,9 @@
 
 var request = require('request');
 var FeedParser = require('feedparser');
-var parser = new FeedParser();
 
 function rss (url, max, callback) {
+  var parser = new FeedParser();
   var c = 0;
   if (max == null)
     max = 10;
@@ -17,7 +17,16 @@ function rss (url, max, callback) {
   var req = request({
     method: 'GET',
     uri: url
-  }).pipe(parser);
+  });
+
+  req.on('response', function (response) {
+    var stream = this;
+
+    if (response.statusCode != 200) 
+      return this.emit('error', new Error('Bad status code'));
+
+    stream.pipe(parser);
+  });
 
   req.on('error', function (err) {
     console.log(err);
@@ -38,10 +47,9 @@ function rss (url, max, callback) {
         res.articles.push(item);
     }
 
-  });
+    if (c === max)
+      return callback(null, res);
 
-  parser.on('end', function () {
-    return callback(null, res);
   });
 
 }
